3cbe6edfe94ed22e1de421e462b7875fc86a4dfd
tadams95 <tyrelleadams28@gmail.com>
2023-01-16 16:25:50 -0800

    Setup HTTPS, SSL, and Private Keys

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
+const fs = require("fs");
 const path = require("path");
+const https = require("https");
 const express = require("express");
 
 const PORT = 3000;
@@ -6,13 +8,18 @@ const PORT = 3000;
 const app = express();
 
 app.get("/secret", (req, res) => {
-    return res.send("Your personal secret value is 222")
-})
+  return res.send("Your personal secret value is 222");
+});
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}...`);
-});
+https
+  .createServer({
+    key: fs.readFileSync("key.pem"),
+    cert: fs.readFileSync("cert.pem"),
+  }, app)
+  .listen(PORT, () => {
+    console.log(`Listening on port ${PORT}...`);
+  });
