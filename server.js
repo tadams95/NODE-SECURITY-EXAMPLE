1b626f73fdedcec148ae92555b8bb5e028753842
tadams95 <tyrelleadams28@gmail.com>
2023-01-18 19:54:58 -0800

    Restrict access to /secret endpoint

--- server.js
+++ server.js
@@ -57,8 +57,9 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 function checkLoggedIn(req, res, next) {
-  const isLoggedIn = true; //TODO
-  if (isLoggedIn) {
+  console.log("Current user is:", req.user);
+  const isLoggedIn = req.isAuthenticated() && req.user;
+  if (!isLoggedIn) {
     return res.status(401).json({
       error: "You must log in.",
     });
